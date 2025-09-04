b627a7dd7284124612d04b6da932af2ec8bdfde7
Ankit <155306694+ankitgamer12@users.noreply.github.com>
2025-09-04 12:09:26 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,10 +11,25 @@ const PORT = process.env.PORT || 5000;
 //cors enable
 app.use(cors());
 
-//example route
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from server!" });
+//backend
+const authRoutes = require("./routes/auth");
+const resumeRoutes = require("./routes/resume");
+const airoutes = require("./routes/ai");
+
+app.use(express.json());
+
+//routes
+app.use("/api/auth", authRoutes);
+app.use("/api/resume", resumeRoutes);
+app.use("/api/ai", airoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
